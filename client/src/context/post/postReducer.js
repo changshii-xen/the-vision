5c0100d93281d911062a74283313e1de4cc539bf
Dominic <dominicwindvogel@gmail.com>
2020-08-20 15:44:13 +0200

    okay

--- client/src/context/post/postReducer.js
+++ client/src/context/post/postReducer.js
@@ -1,71 +1,75 @@
 import {
-    GET_POSTS,
-    ADD_POST,
-    DELETE_POST,
-    SET_CURRENT,
-    CLEAR_CURRENT,
-    UPDATE_POST,
-    FILTER_POSTS,
-    CLEAR_POSTS,
-    CLEAR_FILTER,
-    POST_ERROR
-} from '../types';
+  GET_POSTS,
+  ADD_POST,
+  DELETE_POST,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_POST,
+  FILTER_POSTS,
+  CLEAR_POSTS,
+  CLEAR_FILTER,
+  POST_ERROR,
+} from "../types";
+
+// "heroku-postbuild": "cd client && npm install && npm run build"
 
 export default (state, action) => {
-    switch(action.type){
-        case GET_POSTS:
-            return {
-                ...state,
-                contacts: action.payload,
-                loading: false,
-            };
-        case ADD_POST:
-            return{
-                ...state,
-                posts:[...state.posts, action.payload],
-                loading: false
-            }
-        case DELETE_POST:
-            return{
-                ...state,
-                posts: state.posts.filter(post => post.id !== action.payload),
-                loading: false
-            }
-        case UPDATE_POST:
-            return {
-                ...state,
-                posts: state.posts.map(post => post.id === action.payload.id ? action.payload : post),
-                loading: false 
-            }
-        case SET_CURRENT:
-            return{
-                ...state,
-                current: action.payload
-            };
-        case CLEAR_CURRENT:
-            return{
-                ...state,
-                current: null
-            }
-        case FILTER_POSTS:
-            return{
-                ...state,
-                filtered: state.posts.filter(post =>{
-                    const regex = new RegExp(`${action.payload}`, 'gi');
-                    return post.tag.match(regex) || post.content.match(regex);
-                })
-            }
-        case CLEAR_FILTER:
-            return{
-                ...state,
-                filtered: null
-            };
-        case POST_ERROR:
-            return {
-                ...state,
-                error: action.payload
-            }
-        default: 
-            return state;
-    }
-}
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, action.payload],
+        loading: false,
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post.id !== action.payload),
+        loading: false,
+      };
+    case UPDATE_POST:
+      return {
+        ...state,
+        posts: state.posts.map(
+          post => (post.id === action.payload.id ? action.payload : post)
+        ),
+        loading: false,
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    case FILTER_POSTS:
+      return {
+        ...state,
+        filtered: state.posts.filter(post => {
+          const regex = new RegExp(`${action.payload}`, "gi");
+          return post.tag.match(regex) || post.content.match(regex);
+        }),
+      };
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null,
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
--- server.js
+++ server.js
@@ -9,9 +9,9 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/index.html`));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/client/index.html`));
+// });
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
@@ -20,5 +20,13 @@ app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/posts", require("./routes/posts"));
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

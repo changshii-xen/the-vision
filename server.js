4c1d16c78902f856e3f9dbd260f839d77058d41e
Dominic <dominicwindvogel@gmail.com>
2020-08-20 10:22:15 +0200

    show

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+  res.sendFile(path.join(`${__dirname}/client/index.html`));
 });
 
 // Define Routes

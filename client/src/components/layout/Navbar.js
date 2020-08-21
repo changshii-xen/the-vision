3cef4c2eb9140c863b470b4d4cba07b598bdeae5
Dominic <dominicwindvogel@gmail.com>
2020-08-21 10:12:46 +0200

    lastrun

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,73 +1,91 @@
-import React, {Fragment, useContext} from 'react';
-import Protypes from 'prop-types';
-import logo from '../images/logo.png'
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
-import { Link } from 'react-router-dom';
-import AuthContext from '../../context/auth/authContext'
+import React, { Fragment, useContext } from "react";
+import Protypes from "prop-types";
+import logo from "../images/logo.png";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 
+const Navbar = ({ title }) => {
+  const authContext = useContext(AuthContext);
 
-const Navbar = ({title}) => {
-    const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
 
-    const {isAuthenticated, logout, user} = authContext;
+  const onLogout = () => {
+    logout();
+  };
 
-    const onLogout = () => {
-      logout();
-    }
-
-    const authLinks = (
-      <Fragment>
-      <ul>
-        
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-          <a onClick={onLogout} href="#!">
-          <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
-          </a>
-          </li>
-        
-        </ul>
-        
-      </Fragment>
-    )
-    const guestLinks = (
-      <Fragment>
+  const authLinks = (
+    <Fragment>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+        <i class="material-icons">menu</i>
+      </a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li>
-              <Link to='/register'>Register</Link>
-            </li>
-            <li>
-              <Link to='/login'>Login</Link>
-            </li>
-      </Fragment>
-    )
-    return (
-      <nav className= 'teal'>
-        <div className="nav-wrapper">
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <a onClick={onLogout} href="#!">
+            <i className="fas fa-sign-out-alt" />{" "}
+            <span className="hide-sm">Logout</span>
+          </a>
+        </li>
+      </ul>
+    </Fragment>
+  );
+  const authSideLinks = (
+    <Fragment>
+      <ul class="sidenav" id="mobile-demo">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <a onClick={onLogout} href="#!">
+            <i className="fas fa-sign-out-alt" />{" "}
+            <span className="hide-sm">Logout</span>
+          </a>
+        </li>
+      </ul>
+    </Fragment>
+  );
 
-          <a href="#!" className="brand-logo">{title}</a>
-          <ul class="right hide-on-med-and-down">
-            {isAuthenticated ? authLinks : guestLinks}
-          </ul>
-        </div>
-      </nav>
-    
-      
-    )
-}
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
 
-Navbar.propTypes ={
-    title: Protypes.string.isRequired,
-    
-}
+  return (
+    <nav className="teal">
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo">
+          {title}
+        </a>
+        <ul class="right hide-on-med-and-down">
+          {isAuthenticated ? authLinks : guestLinks}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-Navbar.defaultProps ={
-    title: 'The Vision | TV1',
-}
+Navbar.propTypes = {
+  title: Protypes.string.isRequired,
+};
 
-export default Navbar
+Navbar.defaultProps = {
+  title: "The Vision | TV1",
+};
+
+export default Navbar;
--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -1,77 +1,87 @@
-import React, {useState, useContext, useEffect} from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
-import PostContext from '../../context/post/postContext'
+import React, { useState, useContext, useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import PostContext from "../../context/post/postContext";
 
 const PostForm = () => {
-    const postContext = useContext(PostContext)
+  const postContext = useContext(PostContext);
 
-    const { addPost, current, clearCurrent, updatePost } = postContext;
+  const { addPost, current, clearCurrent, updatePost } = postContext;
 
-    useEffect(() =>{
-        if (current !== null) {
-            setPost(current);
-        } else {
-            setPost({
-                tag: '',
-                content: ''
-            });
-        }
-    }, [postContext, current]);
-
-    const [post, setPost] = useState({
-        tag: '',
-        content: ''
-    });
-
-    const {tag, content} = post;
-
-    const onChange = e => setPost({...post, [e.target.name]: e.target.value});
-
-    const onSubmit = e =>{
-        e.preventDefault();
-        if (current === null) {
-            addPost(post);
-        } else {
-            updatePost(post)
-        }
+  useEffect(
+    () => {
+      if (current !== null) {
+        setPost(current);
+      } else {
         setPost({
-            tag: '',
-            content: ''
+          tag: "",
+          content: "",
         });
-    };
+      }
+    },
+    [postContext, current]
+  );
 
-    const clearAll = () =>{
-        clearCurrent();
+  const [post, setPost] = useState({
+    tag: "",
+    content: "",
+  });
+
+  const { tag, content } = post;
+
+  const onChange = e => setPost({ ...post, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (current === null) {
+      addPost(post);
+    } else {
+      updatePost(post);
     }
-    return (
-        <form onSubmit={onSubmit}>
-            <h4 className="text">{current ? 'Edit Post': 'Write Post'}</h4>
-            <input 
-            type="text" 
-            placeholder="Todays Tag" 
-            name="tag" 
-            value={tag} 
-            onChange={onChange}
-            />
-            <input 
-            type="text" 
-            placeholder="What's your take on today's tag" 
-            name="content" 
-            value={content} 
-            onChange={onChange}
-            />
-            <div>
-                <input type="submit" value={current ? 'Save Post': 'Create Post'} className="btn btn-block"/>
-            </div>
-            {current && (
-                <div>
-                <button className="btn  btn-block" onClick={clearAll}>
-                    Clear
-                </button>
-                </div>
-            )}
-        </form>
-    )
-}
+    setPost({
+      tag: "",
+      content: "",
+    });
+  };
 
-export default PostForm
+  const clearAll = () => {
+    clearCurrent();
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h4 className="text">
+        {current ? "Edit Post" : "Write Post"}
+      </h4>
+      <input
+        type="text"
+        placeholder="Todays Tag"
+        name="tag"
+        value={tag}
+        onChange={onChange}
+        required
+      />
+      <input
+        type="text"
+        placeholder="What's your take on today's tag"
+        name="content"
+        value={content}
+        onChange={onChange}
+        required
+      />
+      <div>
+        <input
+          type="submit"
+          value={current ? "Save Post" : "Create Post"}
+          className="btn btn-block"
+        />
+      </div>
+      {current &&
+        <div>
+          <button className="btn  btn-block" onClick={clearAll}>
+            Clear
+          </button>
+        </div>}
+    </form>
+  );
+};
+
+export default PostForm;
